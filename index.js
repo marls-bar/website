25fb39a1eb5b1ef988b95c3549ac72cbeb155e62
Aidan Marley <a.d.marley@outlook.com>
2023-05-30 19:54:16 +1000

    new intro finalised

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-// index.js
-const path = require('path');
 const express = require('express');
 const app = express();
-const resumeRouter = require('./routes/resume');
+const port = 3000;
 
-// Set EJS as the view engine
 app.set('view engine', 'ejs');
-
-// Set the views directory
-app.set('views', path.join(__dirname, 'views'));
-
-// Middleware
 app.use(express.static('public'));
 
-// Routes
-app.use('/', resumeRouter);
+app.get('/', (req, res) => {
+  res.render('resume');
+});
 
-// Start the server
-const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
